d741333edd4815f7ab2cf969643975e2f3358d38
LR <leeruichn@qq.com>
2019-03-04 17:43:40 +0800

    java代码测试（无）

--- src/test/java/CookieUtilTest.java
+++ src/test/java/CookieUtilTest.java
@@ -4,10 +4,8 @@ import java.util.Date;
 public class CookieUtilTest {
 
 	public static void main(String[] args) throws Exception {
-		SimpleDateFormat mySDF = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		Date date = new Date();
-		Date oldDate1 = mySDF.parse("2012-05-12 15:16:00"); //这里时间可以自己定
-		Date nowDate1 = mySDF.parse(mySDF.format(date));
-		System.out.println(nowDate1.compareTo(oldDate1));
+		Integer max;
+		max=9/5;
+		System.out.println(max);
 	}
 }
