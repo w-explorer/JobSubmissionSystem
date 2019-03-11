7cfe1a78c66c6ef7155df8223f7dc5ecd343d2c5
hongbing1998 <1227911097@qq.com>
2019-03-11 21:28:28 +0800

    删除之前未删除的内容

--- src/main/java/com/cdtu/util/FormatDateToString.java
+++ src/main/java/com/cdtu/util/FormatDateToString.java
@@ -1,7 +1,6 @@
 package com.cdtu.util;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * 将数据库中的date(以秒的形式呈现)转换为String ClassName:描述类
@@ -14,14 +13,5 @@ public class FormatDateToString {
 	public static String fromatData(Object date) {
 		SimpleDateFormat mySDF = new SimpleDateFormat("yyyy-MM-dd");
 		return mySDF.format(date);
-	}
-
-	public static void main(String[] args) {
-		Date curDate = new Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		String start = formatter.format(curDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-		String end = formatter.format(curDate);
-		System.out.println(start);
-		System.out.println(end);
 	}
 }
